5c2571d2b24d8d42b0da86f7afb14d9988244f57
henriquecvieira <hnr01@live.com>
2023-01-15 02:45:16 -0300

    innitial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg"
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          praying to the Lord all the day asking to be more wise, successful and
+          grateful
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          John 3:16
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
